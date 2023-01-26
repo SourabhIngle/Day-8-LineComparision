71083fb5a6816ac4ab8dce898fcca2fef0f37dd8
SourabhIngle <100rabhingle@gmail.com>
2023-01-26 14:40:41 +0530

    Added UC2

--- LineComparison.java
+++ LineComparison.java
@@ -5,17 +5,33 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number for x1 = ");
+
+        System.out.println("First line");
+        System.out.println("Enter the Co-ordinates of x1 and y1 : ");
         int x1 = sc.nextInt();
-        System.out.print("Enter the number for y1 = ");
         int y1 = sc.nextInt();
-
-        System.out.print("Enter the number for x2 = ");
+        System.out.println("Enter the Co-ordinates of x2 and y2 :");
         int x2 = sc.nextInt();
-        System.out.print("Enter the number for y2 = ");
         int y2 = sc.nextInt();
+        Double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
-        double length = Math.sqrt((Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2));
-        System.out.print("Calculated Length = " + length);
+        System.out.println("Second line");
+        System.out.println("Enter the Co-ordinates of x3 and y3 :");
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+        System.out.println("Enter the Co-ordinates of x4 and y4 :");
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+        Double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+
+        System.out.println("Length of the first line = " + length1);
+        System.out.println("Length of the second line = " + length2);
+
+        if (length1.equals(length2))
+            System.out.println("Lines are equal. ");
+        else
+            System.out.println("Lines are not equal.");
     }
 }
+
+
